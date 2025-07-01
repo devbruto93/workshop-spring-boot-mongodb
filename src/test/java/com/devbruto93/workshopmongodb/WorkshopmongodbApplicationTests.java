a7184d13001ef42d91625927f15d14acb1a24622
devbruto93 <douglas.modest.2013@gmail.com>
2025-07-01 12:11:58 -0300

    primeiro commit - projeto criado

---
+++ src/test/java/com/devbruto93/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbruto93.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
