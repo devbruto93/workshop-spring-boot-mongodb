47c756707dbca6b7d984673a27bc870bfa7c1a81
devbruto93 <douglas.modest.2013@gmail.com>
2025-07-07 15:27:15 -0300

    Atualizacao de usuario com o PUT

--- src/main/java/com/devbruto93/workshopmongodb/services/UserService.java
+++ src/main/java/com/devbruto93/workshopmongodb/services/UserService.java
@@ -34,6 +34,18 @@ public class UserService {
         repo.deleteById(id);
     }
 
+    public User update(User obj) { //NO CURSO QUE ESTOU ACOMPANHANDO USARIA O METODO findOne(), POREM ELE ESTA OBSOLETO NO SPRING DATA JPA.
+        User newObj = repo.findById(obj.getId()).orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado. Id: " + obj.getId()));
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
+
     public User fromDTO(UserDTO objDto){
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
