402d10688057873742422d9e6f21ffc4057d5fa1
devbruto93 <douglas.modest.2013@gmail.com>
2025-07-15 07:54:29 -0300

    Obtendo um post por id

---
+++ src/main/java/com/devbruto93/workshopmongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.devbruto93.workshopmongodb.services;
+
+import com.devbruto93.workshopmongodb.Post;
+import com.devbruto93.workshopmongodb.repository.PostRopository;
+import com.devbruto93.workshopmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRopository repo;
+
+    public Post findById(String id){
+        Optional<Post> user = repo.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+    }
+}
