25517a05ea5f16b5bc3bdfc6c782afe069036adf
devbruto93 <douglas.modest.2013@gmail.com>
2025-08-03 00:40:03 -0300

    Consulta simples com o @Query

--- src/main/java/com/devbruto93/workshopmongodb/repository/PostRopository.java
+++ src/main/java/com/devbruto93/workshopmongodb/repository/PostRopository.java
@@ -2,12 +2,16 @@ package com.devbruto93.workshopmongodb.repository;
 
 import com.devbruto93.workshopmongodb.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRopository extends MongoRepository<Post,String> {
+
+    @Query("{'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
